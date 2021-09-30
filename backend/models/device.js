c3b3f31775b191939a2b9139b8767b69704c0433
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-10-01 00:46:53 +0530

    Add GET and POST routes for / in device router

--- backend/models/device.js
+++ backend/models/device.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
-const Device = mongoose.model('Device', new mongoose.Schema({}));
+const Device = mongoose.model(
+  'Device',
+  new mongoose.Schema({
+    deviceType: { type: Number, required: true, min: 1, unique: true },
+    description: { type: String, required: true, trim: true },
+    deviceIDsInUse: { type: Number, required: true, min: 1 },
+  })
+);
+
+function validateDevice(device) {
+  const schema = Joi.object({
+    deviceType: Joi.number().required().min(1),
+    description: Joi.string().required(),
+    deviceIDsInUse: Joi.number().required().min(1),
+  });
+
+  return schema.validate(device);
+}
 
 exports.Device = Device;
+exports.validate = validateDevice;
--- backend/routes/devices.js
+++ backend/routes/devices.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const { Device, validate } = require('../models/device');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello world');
+router.get('/', async (req, res) => {
+  const devices = await Device.find().sort('deviceType');
+  res.status(200).send(devices);
+});
+
+// use superAdmin middleware
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let device = await Device.findOne({ deviceType: req.body.deviceType });
+  if (device)
+    return res
+      .status(400)
+      .send(`Device with deviceType ${req.body.deviceType} already exists`);
+
+  device = new Device({
+    deviceType: req.body.deviceType,
+    description: req.body.description,
+    deviceIDsInUse: req.body.deviceIDsInUse,
+  });
+  await device.save();
+
+  res.status(200).send(device);
 });
 
 module.exports = router;
