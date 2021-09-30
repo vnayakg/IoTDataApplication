543e57d6466ba662c47fd18b346c9abd71730200
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-09-30 23:27:03 +0530

    Add default port incase PORT is not defined in .env

--- backend/app.js
+++ backend/app.js
@@ -11,5 +11,5 @@ app.use(express.json());
 // routes
 app.use('/devices', devices);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
