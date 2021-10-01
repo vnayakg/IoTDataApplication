99568dece7c9fcc24df7f8204f1864a23795402f
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-10-01 19:44:12 +0530

    Added error caching to be used with express-async-errors

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const morgan = require('morgan');
 
 require('./config/database').connect();
 const devices = require('./routes/devices');
+const error = require('./middleware/error');
 
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(error); // should be last
 
 // routes
 app.use('/devices', devices);
