c0cdba106225e1ce759e7820733e297103c67e57
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-10-07 18:58:27 +0530

    Added superAdmin to /devices routes

--- backend/routes/devices.js
+++ backend/routes/devices.js
@@ -3,31 +3,35 @@ const { Device, validateDevice } = require('../models/device');
 const validate = require('../middleware/validate');
 const validateObjectId = require('../middleware/validateObjectId');
 const auth = require('../middleware/auth');
+const superAdmin = require('../middleware/superAdmin');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   const devices = await Device.find().sort('deviceType');
   res.status(200).send(devices);
 });
 
-// use superAdmin middleware
-router.post('/', validate(validateDevice), async (req, res) => {
-  let device = await Device.findOne({ deviceType: req.body.deviceType });
-  if (device)
-    return res
-      .status(400)
-      .send(`Device with deviceType ${req.body.deviceType} already exists`);
+router.post(
+  '/',
+  [auth, superAdmin, validate(validateDevice)],
+  async (req, res) => {
+    let device = await Device.findOne({ deviceType: req.body.deviceType });
+    if (device)
+      return res
+        .status(400)
+        .send(`Device with deviceType ${req.body.deviceType} already exists`);
 
-  device = new Device({
-    deviceType: req.body.deviceType,
-    description: req.body.description,
-    deviceIDsInUse: req.body.deviceIDsInUse,
-  });
-  await device.save();
+    device = new Device({
+      deviceType: req.body.deviceType,
+      description: req.body.description,
+      deviceIDsInUse: req.body.deviceIDsInUse,
+    });
+    await device.save();
 
-  res.status(200).send(device);
-});
+    res.status(200).send(device);
+  }
+);
 
 // deviceId = ObjectId of Device document
 router.get('/:deviceId', validateObjectId('deviceId'), async (req, res) => {
@@ -39,11 +43,10 @@ router.get('/:deviceId', validateObjectId('deviceId'), async (req, res) => {
   res.status(200).send(device);
 });
 
-// use superAdmin middleware
 // deviceId = ObjectId of Device document
 router.put(
   '/:deviceId',
-  [validateObjectId('deviceId'), validate(validateDevice)],
+  [auth, superAdmin, validateObjectId('deviceId'), validate(validateDevice)],
   async (req, res) => {
     let device = await Device.findOne({ deviceType: req.body.deviceType });
     if (device && device._id === req.param.deviceId)
@@ -68,15 +71,18 @@ router.put(
   }
 );
 
-// use superAdmin middleware
 // deviceId = ObjectId of Device document
-router.delete('/:deviceId', validateObjectId('deviceId'), async (req, res) => {
-  const device = await Device.findByIdAndDelete(req.params.deviceId);
+router.delete(
+  '/:deviceId',
+  [auth, superAdmin, validateObjectId('deviceId')],
+  async (req, res) => {
+    const device = await Device.findByIdAndDelete(req.params.deviceId);
 
-  if (!device)
-    return res.status(404).send('The device with given ID was not found');
+    if (!device)
+      return res.status(404).send('The device with given ID was not found');
 
-  res.status(200).send(device);
-});
+    res.status(200).send(device);
+  }
+);
 
 module.exports = router;
