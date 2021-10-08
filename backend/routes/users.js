7abce00b896975f07558d960f495e85a0f3e1088
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-10-08 14:20:15 +0530

    Fixed bug and removed password from returning

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
+const express = require('express');
+const bcrypt = require('bcryptjs');
 
-const { User, validateRegister, validateUpdate } = require("../models/user");
-const validate = require("../middleware/validate");
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
+const { User, validateRegister, validateUpdate } = require('../models/user');
+const validate = require('../middleware/validate');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 const router = express.Router();
 
 router.post(
-  "/register",
+  '/register',
   [auth, admin, validate(validateRegister)],
   async (req, res) => {
     const { username, password, name, phone, isAdmin } = req.body;
@@ -43,40 +43,49 @@ router.post(
     res.status(200).send({ _id: user._id, username, name });
   }
 );
+
 //reset password
-router.post("/reset", auth, async (req, res) => {
+router.post('/reset', auth, async (req, res) => {
   const { _id, username } = req.user;
   const { password, newPassword } = req.body;
 
   if (password === newPassword)
     return res
       .status(400)
-      .send("Current password and new password should be different");
+      .send('Current password and new password should be different');
 
   const user = await User.findOne({ username });
 
   const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword) return res.status(400).send("Invalid current password");
+  if (!validPassword) return res.status(400).send('Invalid current password');
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(newPassword, salt);
 
-  const updatedUser = await User.findByIdAndUpdate(_id, { password: hash });
+  const updatedUser = await User.findByIdAndUpdate(_id, {
+    password: hash,
+  }).select('-password');
 
   res.status(200).send(updatedUser);
 });
+
 // update user info
-router.put("/update", [auth, validate(validateUpdate)], async (req, res) => {
+router.put('/update', [auth, validate(validateUpdate)], async (req, res) => {
   const { _id } = req.user;
   const { name, phone } = req.body;
 
-  let user = await User.findByIdAndUpdate(_id, { name, phone }, { new: true });
+  let user = await User.findByIdAndUpdate(
+    _id,
+    { name, phone },
+    { new: true }
+  ).select('-password');
 
   res.status(200).send(user);
 });
+
 // get all users info
-router.get("/", [auth, admin], async (req, res) => {
-  let users = await Users.find();
+router.get('/', [auth, admin], async (req, res) => {
+  let users = await User.find().select('-password');
 
   res.status(200).send(users);
 });
